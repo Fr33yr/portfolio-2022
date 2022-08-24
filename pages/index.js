e28e73b2caf676de1978f90fe73aa00e705084b1
Agustin Cristobo <fr33yr@gmail.com>
2022-08-24 19:26:00 -0300

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import Head from 'next/head'
 import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -11,58 +14,70 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <header className={styles.header}>
+        <nav className={styles.nav}>
+          <ul>
+            <li>Home</li>
+            <li>Skills</li>
+            <li>Projects</li>
+          </ul>
+        </nav>
+        <h1>Hello there, I am
+          Agustin</h1>
+        <h2>Frontend developer</h2>
+      </header>
+
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <article>
+          <h2>What i do</h2>
+          <p>I love every part of the development, from wireframing with
+            paper and pencil then the ui design and the muckup until writing
+            the code of the application or website</p>
+        </article>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <article className={styles.skills}>
+          <h2>Skills</h2>
+          <p>I love every part of the development, from wireframing with
+            paper and pencil then the ui design and the muckup until writing
+            the code of the application or website</p>
+        </article>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <article className={styles.projects}>
+          <h2>Projects</h2>
+          <p>I love every part of the development, from wireframing with
+            paper and pencil then the ui design and the muckup until writing
+            the code of the application or website</p>
+        </article>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+
+        <div className={styles.medialinks}>
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </div>
+        <p>
+          updated 2022
+        </p>
       </footer>
     </div>
   )
