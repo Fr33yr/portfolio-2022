5396a8dacb87b056e0501842398e2ce5f763a448
Agustin Cristobo <fr33yr@gmail.com>
2022-08-28 10:15:01 -0300

    mobile menu

--- components/layout/header/Header.jsx
+++ components/layout/header/Header.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Hamburger from 'hamburger-react'
 import styles from './Header.module.css'
 
 export default function Header() {
+    const [isNavAvtive, setIsNavActive] = useState(false)
+
+    const handleToggle = (toggled) => {
+        if (toggled) {
+            setIsNavActive(true)
+        } else {
+            setIsNavActive(false)
+        }
+    }
+
     return (
         <>
             <header className={styles.header}>
                 <nav className={styles.nav}>
-                    <ul>
+                    <ul style={isNavAvtive? {minHeight: "100vh", transform: "translate(0px, 0px)", display: "flex"} : {}}>
                         <li>Home</li>
                         <li>Skills</li>
                         <li>Projects</li>
+                        <li>Contact me</li>
                     </ul>
+                    <div className={styles.hamburger}>
+                        <Hamburger
+                            color='#fff'
+                            size={24}
+                            direction="right"
+                            onToggle={handleToggle} />
+                    </div>
                 </nav>
                 <h1>
                     Hello there, I am
